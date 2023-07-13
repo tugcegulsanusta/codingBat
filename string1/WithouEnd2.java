aa55bd3c1d22ee723487716da46437ed4718e0e9
Tuğçe Usta <tugce.gulsan@gmail.com>
2023-07-13 09:35:14 +0300

    string1 solutions

---
+++ string1/WithouEnd2.java
@@ -0,0 +1,19 @@
+package string1;
+
+public class WithouEnd2 {
+    /*
+    Given a string, return a version without both the first and last char of the string. The string may be any length, including 0.
+
+
+    withouEnd2("Hello") → "ell"
+    withouEnd2("abc") → "b"
+    withouEnd2("ab") → ""
+     */
+    public String withouEnd2(String str) {
+        if(str.length()>=2){
+            return str.substring(1,str.length()-1);
+        }
+        return "";
+    }
+
+}
