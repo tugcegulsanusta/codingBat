20e9d98469c995723361876630ab8df50f95a204
Tuğçe Usta <tugce.gulsan@gmail.com>
2023-07-26 16:14:46 +0300

    array1 solutions

---
+++ array1/FirstLast6.java
@@ -0,0 +1,18 @@
+package array1;
+
+public class FirstLast6 {
+    /*
+    Given an array of ints, return true if 6 appears as either the first or last element in the array. The array will be length 1 or more.
+
+
+    firstLast6([1, 2, 6]) → true
+    firstLast6([6, 1, 2, 3]) → true
+    firstLast6([13, 6, 1, 2, 3]) → false
+     */
+    public boolean firstLast6(int[] nums) {
+        if((nums.length>=1)&&((nums[0] ==6) ||(nums[nums.length-1]==6))){
+            return true;
+        }return false;
+    }
+
+}
