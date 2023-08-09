b4612a25e731606962764c5fb9e68ee4f6179024
Tuğçe Usta <tugce.gulsan@gmail.com>
2023-08-09 18:20:52 +0300

    array1 solutions

---
+++ array1/FrontPiece.java
@@ -0,0 +1,21 @@
+package array1;
+
+public class FrontPiece {
+    /*
+    Given an int array of any length, return a new array of its first 2 elements. If the array is smaller than length 2, use whatever elements are present.
+
+
+    frontPiece([1, 2, 3]) → [1, 2]
+    frontPiece([1, 2]) → [1, 2]
+    frontPiece([1]) → [1]
+     */
+    public int[] frontPiece(int[] nums) {
+        int[] arr=new int[2];
+        if(nums.length>=2){
+            arr[0] = nums[0];
+            arr[1] = nums[1];
+            return arr;
+        }return nums;
+    }
+
+}
