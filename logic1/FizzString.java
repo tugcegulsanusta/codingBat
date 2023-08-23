5d70663ea572aaa930749cd385f9f28fe0905e99
Tuğçe Usta <tugce.gulsan@gmail.com>
2023-08-23 10:50:41 +0300

    logic1 solutions

--- logic1/FizzString.java
+++ logic1/FizzString.java
@@ -1,2 +1,19 @@
-package logic1;public class FizzString {
+package logic1;
+
+public class FizzString {
+    /*
+    Given a string str, if the string starts with "f" return "Fizz". If the string ends with "b" return "Buzz". If both the "f" and "b" conditions are true, return "FizzBuzz". In all other cases, return the string unchanged. (See also: FizzBuzz Code)
+
+
+    fizzString("fig") → "Fizz"
+    fizzString("dib") → "Buzz"
+    fizzString("fib") → "FizzBuzz"
+     */
+    public String fizzString(String str) {
+        if((str.substring(0,1).equals("f")) && (str.substring(str.length()-1,str.length()).equals("b")))return "FizzBuzz";
+        if(str.substring(0,1).equals("f"))return "Fizz";
+        if(str.substring(str.length()-1,str.length()).equals("b")) return "Buzz";
+        else return str;
+    }
+
 }
