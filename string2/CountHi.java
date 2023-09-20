ca48eca65955f5d8867177d43de22e8943257535
Tuğçe Usta <tugce.gulsan@gmail.com>
2023-09-20 15:46:01 +0300

    string2 solutions

---
+++ string2/CountHi.java
@@ -0,0 +1,21 @@
+package string2;
+
+public class CountHi {
+    /*
+    Return the number of times that the string "hi" appears anywhere in the given string.
+
+
+    countHi("abc hi ho") → 1
+    countHi("ABChi hi") → 2
+    countHi("hihi") → 2
+     */
+    public int countHi(String str) {
+        int count = 0;
+        for(int i = 0 ; i<= str.length()-2; i++){
+            if(str.substring(i,i+2).equals("hi")){
+                count++;
+            }
+        }return count;
+    }
+
+}
